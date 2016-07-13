ef2480e0b911e8fa4b177fa13831addab8b65f1c
Alvin <alvin.zablan62@myhunter.cuny.edu>
2016-07-13 19:30:28 -0400

    init

---
+++ statWatch.py
@@ -0,0 +1,39 @@
+import discord
+import asyncio
+import stats
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Bot logged in as ' + client.user.name + ' ' + client.user.id + '.')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!swget'):
+        print('getting!')
+        args = message.content.split(' ')
+        if len(args) != 2:
+            response = 'to get player stats use: \'!swget <BattleTag>\', for example: \'!statWatch Surefour-2559\''
+            await client.send_message(message.channel, response)
+            return
+        tmp = await client.send_message(message.channel, '*Retrieving ' + args[1] + '\'s stats...*')
+        response = await stats.getStats(args[1])
+        await client.edit_message(tmp, response)
+    elif message.content.startswith('!swadd'):
+        print('adding!')
+        args = message.content.split(' ')
+        if len(args) != 2:
+            response = 'to add player to leaderboard use: \'!swadd <BattleTag>\', for example: \'!swadd Surefour-2559\''
+            await client.send_message(message.channel, response)
+            return
+        tmp = await client.send_message(message.channel, '*Retrieving ' + args[1] + '\'s stats...*')
+        if await stats.addPlayer(args[1]):
+            await client.edit_message(tmp, args[1] + ' added to leaderboard.')
+        else:
+            await client.edit_message(tmp, 'Failed to add ' + args[1] + ' to leaderboard.')
+    elif message.content.startswith('!swleaderboard'):
+        response = await stats.getLeaderboard()
+        await client.send_message(message.channel, response)
+
+client.run('azablan.dev@gmail.com', password)
---
+++ stats.py
@@ -0,0 +1,60 @@
+import urllib.request
+import json
+import asyncio
+
+order = ['Level', 'Rank', 'Games', 'Win Rate', 'K/D Ratio']
+
+players = []
+
+def statsToString(stats):
+	statStr = '**' + stats['BattleTag'] + '**' + '\n'
+	for name in order:
+		statStr += '***' + name + ':*** ' + stats[name] + ', '
+	statStr = statStr[:-2]
+	return statStr
+
+def processStats(stats):
+	overallStats = stats['overall_stats']
+	avgStats = stats['average_stats']
+	gameStats = stats['game_stats']
+	newStats = {}
+	newStats['Rank'] = str(overallStats['comprank'])
+	newStats['Games'] = str(overallStats['games'])
+	newStats['Level'] = str(overallStats['prestige']) + ' - ' +str(overallStats['level'])
+	winRate = round(overallStats['wins'] / (overallStats['wins'] + overallStats['losses']), 3) * 100
+	newStats['Win Rate'] = str(round(winRate, 1)) + '%'
+	newStats['K/D Ratio'] = str(gameStats['kpd'])
+	newStats['BattleTag'] = stats['battletag']
+	statsToString(newStats)
+	return newStats
+
+async def apiRequest(battleTag):
+	print('requesting ' + battleTag)
+	url = 'https://owapi.net/api/v2/u/' + battleTag + '/stats/general'
+	try:
+		data = urllib.request.urlopen(url).read()
+		data = data.decode('utf-8')
+		return json.loads(data)
+	except urllib.error.HTTPError:
+		print('request failed')
+		return None
+
+async def getStats(battleTag):
+	playerStats = await apiRequest(battleTag)
+	playerStats = processStats(playerStats)
+	return statsToString(playerStats)
+
+async def addPlayer(battleTag):
+	playerStats = await apiRequest(battleTag)
+	if playerStats is None:
+		return False
+	playerStats = processStats(playerStats)
+	players.append(playerStats)
+	print(players)
+	return True
+
+async def getLeaderboard():
+	statStrs = [statsToString(s) for s in players]
+	print(statStrs)
+	return '\n\n'.join(statStrs)
+
